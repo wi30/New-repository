a46a202030776e711772c0e6b85a85cff1403ebb
wi30 <00001090956423sp@al.educacao.sp.gov.br>
2024-08-15 22:33:02 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(660, 600);
+    background("black");
+    }
+    
+    function draw () {
+    stroke("blue");
+    fill("red" );
+      
+    if (mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
+    }
+    }
